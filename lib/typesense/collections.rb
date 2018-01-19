010f37d6dc733e2172f0e7a8261f68326152d196
Jason Bosco <mail@jasonbos.co>
2018-01-19 07:39:27 -0800

    Dry up code a bit

--- lib/typesense/collections.rb
+++ lib/typesense/collections.rb
@@ -2,24 +2,26 @@ module Typesense
   class Collections
     ENDPOINT_PATH = '/collections'
 
-    def self.create(schema)
-      ApiCall.new.post(ENDPOINT_PATH, schema)
-    end
+    class << self
+      def create(schema)
+        ApiCall.new.post(ENDPOINT_PATH, schema)
+      end
 
-    def self.retrieve(collection_name)
-      ApiCall.new.get("#{ENDPOINT_PATH}/#{collection_name}")
-    end
+      def retrieve(collection_name)
+        ApiCall.new.get("#{ENDPOINT_PATH}/#{collection_name}")
+      end
 
-    def self.delete(collection_name)
-      ApiCall.new.delete("#{ENDPOINT_PATH}/#{collection_name}")
-    end
+      def delete(collection_name)
+        ApiCall.new.delete("#{ENDPOINT_PATH}/#{collection_name}")
+      end
 
-    def self.retrieve_all
-      ApiCall.new.get("#{ENDPOINT_PATH}")
-    end
+      def retrieve_all
+        ApiCall.new.get("#{ENDPOINT_PATH}")
+      end
 
-    def self.documents_path_for(collection_name)
-      "#{ENDPOINT_PATH}/#{collection_name}/documents"
+      def documents_path_for(collection_name)
+        "#{ENDPOINT_PATH}/#{collection_name}/documents"
+      end
     end
   end
 end
--- lib/typesense/documents.rb
+++ lib/typesense/documents.rb
@@ -1,27 +1,29 @@
 module Typesense
   class Documents
-    def self.create(collection_name, document)
-      ApiCall.new.post(Collections.documents_path_for(collection_name), document)
-    end
-
-    def self.retrieve(collection_name, document_id)
-      ApiCall.new.get("#{Collections.documents_path_for(collection_name)}/#{document_id}")
-    end
-
-    def self.delete(collection_name, document_id)
-      ApiCall.new.delete("#{Collections.documents_path_for(collection_name)}/#{document_id}")
-    end
-
-    def self.export(collection_name)
-      api_response = ApiCall.new.get_unparsed_response("#{Collections.documents_path_for(collection_name)}/export")
-
-      api_response.split("\n").map do |document_json_string|
-        JSON.parse(document_json_string)
+    class << self
+      def create(collection_name, document)
+        ApiCall.new.post(Collections.documents_path_for(collection_name), document)
       end
-    end
 
-    def self.search(collection_name, search_parameters)
-      ApiCall.new.get("#{Collections.documents_path_for(collection_name)}/search", search_parameters)
+      def retrieve(collection_name, document_id)
+        ApiCall.new.get("#{Collections.documents_path_for(collection_name)}/#{document_id}")
+      end
+
+      def delete(collection_name, document_id)
+        ApiCall.new.delete("#{Collections.documents_path_for(collection_name)}/#{document_id}")
+      end
+
+      def export(collection_name)
+        api_response = ApiCall.new.get_unparsed_response("#{Collections.documents_path_for(collection_name)}/export")
+
+        api_response.split("\n").map do |document_json_string|
+          JSON.parse(document_json_string)
+        end
+      end
+
+      def search(collection_name, search_parameters)
+        ApiCall.new.get("#{Collections.documents_path_for(collection_name)}/search", search_parameters)
+      end
     end
   end
 end
