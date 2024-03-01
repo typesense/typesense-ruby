cc6f97ad03b93ddc17d97266a73760148d2dc7de
Jason Bosco <mail@jasonbos.co>
2024-03-01 13:41:15 -0600

    Fix linting issue

--- spec/typesense_spec.rb
+++ spec/typesense_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 describe Typesense do
   it 'has a version number' do
-    expect(Typesense::VERSION).not_to be_nil
+    expect(described_class::VERSION).not_to be_nil
   end
 end
