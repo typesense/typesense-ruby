cc3ee417d172bbeba39cb593936cb6a3a99c9c7f
Jason Bosco <mail@jasonbos.co>
2025-08-14 18:34:05 -0500

    Start typesense locally automatically for integration tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,62 @@ end
 require 'bundler/setup'
 require 'webmock/rspec'
 require 'typesense'
+require 'faraday'
+
+WebMock.disable_net_connect!(allow_localhost: true)
+
+def typesense_healthy?(host = 'localhost', port = 8108)
+  conn = Faraday.new("http://#{host}:#{port}")
+  response = conn.get('/health')
+  response.status == 200 && response.body.include?('ok')
+rescue StandardError
+  false
+end
+
+def start_typesense_if_needed
+  if typesense_healthy?
+    puts '✅ Typesense is already running and healthy, ready for use in integration tests'
+    return false
+  end
+
+  # Check if Docker is running
+  unless system('docker info > /dev/null 2>&1')
+    raise 'Docker daemon is not running. Please start Docker and try again.'
+  end
+
+  puts 'Starting Typesense with docker-compose...'
+  unless system('docker-compose up -d')
+    raise 'Failed to start docker-compose'
+  end
+
+  # Wait for Typesense to be ready
+  print 'Waiting for Typesense to start'
+  20.times do
+    break if typesense_healthy?
+
+    print '.'
+    sleep 1
+  end
+  puts
+
+  unless typesense_healthy?
+    raise 'Failed to start Typesense - health endpoint did not return OK'
+  end
+
+  puts 'Typesense is ready!'
+  $typesense_started_by_tests = true
+  true
+end
+
+def stop_typesense_if_started
+  unless $typesense_started_by_tests
+    puts "\e[33m\nTest suite did not shut down Typesense automatically, because it was already running when tests started\e[0m"
+    return
+  end
+
+  puts 'Stopping Typesense...'
+  system('docker-compose down')
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -31,4 +87,13 @@ RSpec.configure do |config|
   # It causes the host group and examples to inherit metadata
   # from the shared context.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:suite) do
+    $typesense_started_by_tests = start_typesense_if_needed
+    WebMock.disable_net_connect!
+  end
+
+  config.after(:suite) do
+    stop_typesense_if_started
+  end
 end
