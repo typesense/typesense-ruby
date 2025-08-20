ae74aca9ea987f9a33900c313cbc2c998296ec2e
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-08-20 15:46:01 +0300

    feat(synonyms): add synonym set classes

---
+++ lib/typesense/synonym_set.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Typesense
+  class SynonymSet
+    def initialize(synonym_set_name, api_call)
+      @synonym_set_name = synonym_set_name
+      @api_call = api_call
+    end
+
+    def retrieve
+      @api_call.get(endpoint_path)
+    end
+
+    def delete
+      @api_call.delete(endpoint_path)
+    end
+
+    private
+
+    def endpoint_path
+      "#{SynonymSets::RESOURCE_PATH}/#{URI.encode_www_form_component(@synonym_set_name)}"
+    end
+  end
+end
